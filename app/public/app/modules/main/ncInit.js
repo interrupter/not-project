dc0ef08cff64cc0f54b4e46be212bdbc981eb9ec
Aleksander Repin <alex.repin@mail.ru>
2017-12-09 12:42:14 +0400

    v2.0.0

--- app/public/app/modules/main/ncInit.js
+++
@@ -1,3 +0,0 @@
-var ncInit = function (app, params) {
-    var not = this;
-}
--- app/routes/admin.js
+++
@@ -1,8 +0,0 @@
-var HttpError = require('not-node').Error.Http,
-    async = require('async'),
-    config = require('../config'),
-    extend = require('extend');
-
-exports.main = function(req, res){
-    res.render('dashboard');
-};
